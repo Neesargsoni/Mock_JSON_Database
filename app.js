a2276f51b6cba7c6643385494697184377b9ae4b
Neesargsoni <neesargs@gmail.com>
2025-08-07 18:41:34 -0600

    Changes

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+import express from 'express';
+const app = express();
+require('dotenv').config();
+import { connect } from 'require';
+const PORT = process.env.PORT;
+const uri = process.env.MONGO_URI;
 
+connect(uri).then(
+    async () => {
+        console.log('connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`Conneted to port:${PORT}`);
+        });
+    }
+).
+catch((err)=>{
+  console.log(`Error:${error}`)})
