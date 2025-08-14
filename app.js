6889ea2d7025b4eca7081d2aedb64ed887f7ba73
Neesargsoni <neesargs@gmail.com>
2025-08-13 18:09:59 -0600

    databse connected

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-import express from 'express';
+const express = require('express')
 const app = express();
 require('dotenv').config();
-import { connect } from 'require';
+const mongoose = require('mongoose')
+
 const PORT = process.env.PORT;
 const uri = process.env.MONGO_URI;
 
-connect(uri).then(
+mongoose.connect(uri).then(
     async () => {
         console.log('connected to MongoDB');
         app.listen(PORT, () => {
