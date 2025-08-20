4a6d9ca55d25e6c1e936e155535bbcb55c27deeb
Neesargsoni <neesargs@gmail.com>
2025-08-20 08:50:29 -0600

    updates

--- app.js
+++ app.js
@@ -5,7 +5,19 @@ const mongoose = require('mongoose')
 
 const PORT = process.env.PORT;
 const uri = process.env.MONGO_URI;
+const ejs = require('ejs');
 
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
+app.use(express.urlencoded({extended : true}))
+app.use(express.json());
+app.use(express.static('public'))
+
+app.get('/home',(req,res)=>{
+    res.render('home')
+})
 mongoose.connect(uri).then(
     async () => {
         console.log('connected to MongoDB');
